0254535c7ef452f2feb435bce6213ba28ae75958
sesdf <wltmdfuf123@naver.com>
2018-04-13 17:13:41 +0900

    imgSlide

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function () {
-    var current;
-    var imgSlide = $(".imgSlide > img");
-    setInterval();
+    var next = 1;
+    var imgSlide = $(".imgSlide>img");
+    var max = imgSlide.length-1;
 
+    imgSlide.eq(max).animate({left:800},0);
+    imgSlide.eq(1).animate({left:800},0);
 
+    function slide() {
+        imgSlide.eq(next).animate({left:-800},0);
+        imgSlide.eq(next -1).animate({left:800},1000);
+        imgSlide.eq(next).animate({left:0},1000);
+
+        next++;
+        if(next > max)
+            next = 0;
+    }
+    setInterval(slide,3000);
 });
