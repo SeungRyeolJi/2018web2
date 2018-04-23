af06ef6e07b601f63dfaa7bb2420c385fe399cde
sesdf <wltmdfuf123@naver.com>
2018-04-23 15:48:52 +0900

    imgSlide control finish

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -9,6 +9,24 @@ $(document).ready(function () {
 
     var slideCurrent = setInterval(slide,intervalTime);
 
+    $("#loginButton").click(function () {
+        $("#login_Popup").css({'display':'block'});
+        $("#mask").css({'display':'blcok'});
+    });
+    $("#close_login").click(function () {
+        $("#login_Popup").css({'display': 'none'});
+        $("#mask").css({'display': 'none'});
+    });
+
+    $("#joinButton").click(function () {
+        $("#join_Popup").css({'display':'block'});
+        $("#mask").css({'display':'blcok'});
+    })
+    $("#close_join").click(function () {
+        $("#join_Popup").css({'display': 'none'});
+        $("#mask").css({'display': 'none'});
+    })
+    
     function slide() {
         imgSlide.eq(next).animate({left:-800},0);
         imgSlide.eq(next -1).animate({left:800},1000);
@@ -22,9 +40,9 @@ $(document).ready(function () {
         next--;
         if(next < 0 )
             next = max;
-        imgSlide.eq(next).animate({left:800},0);
-        imgSlide.eq(next -1).animate({left:-800},1000);
-        imgSlide.eq(next).animate({left:0},1000);
+        imgSlide.eq(next-1).animate({left:800},0);
+        imgSlide.eq(next).animate({left:-800},1000);
+        imgSlide.eq(next -1).animate({left:0},1000);
     }
 
     $("#left_arrow").hover(function () {
@@ -33,10 +51,10 @@ $(document).ready(function () {
         $(this).attr('src','img/left.png');
     });
     $("#left_arrow").click(function () {
-        if( st.attr('src') == "img/stop.png") {
+        if(  $("#stop_start").attr('src') == "img/stop.png") {
             clearInterval(slideCurrent);
             slide();
-            slideCurrent = setInterval(slide(), intervalTime);
+            slideCurrent = setInterval(slide, intervalTime);
         }else{
             slide();
         }
@@ -48,17 +66,17 @@ $(document).ready(function () {
         $(this).attr('src','img/right.png');
     });
     $("#right_arrow").click(function () {
-        if( st.attr('src') == "img/stop.png") {
+        if( $("#stop_start").attr('src') == "img/stop.png") {
             clearInterval(slideCurrent);
             minusSlide();
-            slideCurrent = setInterval(slide(), intervalTime);
+            slideCurrent = setInterval(slide, intervalTime);
         }else{
             minusSlide();
         }
     });
 
     $("#stop_start").click(function () {
-        var st = $("#stop_start");
+        var st = $(this);
         if( st.attr('src') == "img/stop.png") {
             st.attr('src', 'img/start.png');
             clearInterval(slideCurrent);
