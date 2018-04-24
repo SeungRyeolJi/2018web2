1c8228985ad9c10e367cba784f4607efe28b505f
www <wltmdfuf123@naver.com>
2018-04-24 14:34:33 +0900

    info box

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -9,10 +9,33 @@ $(document).ready(function () {
 
     var slideCurrent = setInterval(slide,intervalTime);
 
+    $(".infoBox>button:nth-child(1)").click(function () {
+        $(this).css({'color':'#666bff'});
+        infoBoxChange('n');
+    });
+    $(".infoBox>button:nth-child(2)").click(function () {
+        infoBoxChange('c');
+    });
+
+    function infoBoxChange(result){
+        if(result == 'n'){
+            $(".infoBox>button:nth-child(1)").css({'color':'#666bff'});
+            $(".infoBox>button:nth-child(2)").css({'color':'#000000'});
+            $(".notice").css({'display':'block'});
+            $(".cmuDoc").css({'display':'none'});
+        }
+        else if(result=='c'){
+            $(".infoBox>button:nth-child(2)").css({'color':'#666bff'});
+            $(".infoBox>button:nth-child(1)").css({'color':'#000000'});
+            $(".cmuDoc").css({'display':'block'});
+            $(".notice").css({'display':'none'});
+        }
+    }
 
     $("#loginButton").click(function () {
         $("#login_Popup").css({'display':'block'});
         $("#mask").css({'display':'blcok'});
+        $("#join_Popup").css({'display': 'none'});
     });
     $("#close_login").click(function () {
         $("#login_Popup").css({'display': 'none'});
@@ -22,11 +45,12 @@ $(document).ready(function () {
     $("#joinButton").click(function () {
         $("#join_Popup").css({'display':'block'});
         $("#mask").css({'display':'blcok'});
-    })
+        $("#login_Popup").css({'display': 'none'});
+    });
     $("#close_join").click(function () {
         $("#join_Popup").css({'display': 'none'});
         $("#mask").css({'display': 'none'});
-    })
+    });
     
     function slide() {
         imgSlide.eq(next).animate({left:-800},0);
@@ -44,7 +68,7 @@ $(document).ready(function () {
         imgSlide.eq(next-1).animate({left:800},0);
         imgSlide.eq(next).animate({left:-800},1000);
         imgSlide.eq(next -1).animate({left:0},1000);
-    }
+    };
 
     $("#left_arrow").hover(function () {
         $(this).attr('src','img/left_hover.png');
@@ -81,6 +105,7 @@ $(document).ready(function () {
         if( st.attr('src') == "img/stop.png") {
             st.attr('src', 'img/start.png');
             clearInterval(slideCurrent);
+            return false;
         }
         else {
             slideCurrent = setInterval(slide,intervalTime);
